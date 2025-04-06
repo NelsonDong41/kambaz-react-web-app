c9c7a3cf7452c56c110e52f31aeeb3aab2ef864a
unknown <nelsondong158@gmail.com>
2025-04-05 21:48:02 -0400

    typo

--- src/Kambaz/Courses/People/Details.tsx
+++ src/Kambaz/Courses/People/Details.tsx
@@ -3,7 +3,7 @@ import { FaPencil } from "react-icons/fa6";
 import { FaCheck, FaUserCircle } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
 import { useParams, useNavigate } from "react-router";
-=import * as client from "../../Account/client";
+import * as client from "../../Account/client";
 import { FormControl } from "react-bootstrap";
 export default function PeopleDetails() {
     const { uid } = useParams();
